ca931374a0e56d32f8612d868e488fc5cecf83e7
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-13 16:42:16 +0800

    play with menu

--- src/main/java/com/github/zxh/classpy/gui/ClasspyApp.java
+++ src/main/java/com/github/zxh/classpy/gui/ClasspyApp.java
@@ -1,11 +1,11 @@
 package com.github.zxh.classpy.gui;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 /**
@@ -16,22 +16,40 @@ public class ClasspyApp extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Hello World");
-        Group root = new Group();
+        stage.setTitle("Classpy");
+        
+        BorderPane root = new BorderPane();
+        
+        
+        root.setTop(createMenuBar());
+        
+        
         Scene scene = new Scene(root, 300, 250);
-        Button btn = new Button();
-        btn.setLayoutX(100);
-        btn.setLayoutY(80);
-        btn.setText("Hello World");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World");
-            }
-        });
-        root.getChildren().add(btn);
+//        Group root = new Group();
+//        Button btn = new Button();
+//        btn.setLayoutX(100);
+//        btn.setLayoutY(80);
+//        btn.setText("Hello World");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World");
+//            }
+//        });
+//        root.getChildren().add(btn);
         stage.setScene(scene);
         stage.show();
     }
+    
+    private MenuBar createMenuBar() {
+        MenuBar menuBar = new MenuBar();
+        Menu fileMenu = new Menu("File");
+        menuBar.getMenus().add(fileMenu);
+        MenuItem openMenuItem = new MenuItem("Open...");
+        fileMenu.getItems().add(openMenuItem);
+        
+        return menuBar;
+    }
+    
     
     public static void main(String[] args) {
         Application.launch(args);
