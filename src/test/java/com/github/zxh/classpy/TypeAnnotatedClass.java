1b8be696c38b704942588ca9df5c5917c4839bad
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-15 16:50:46 +0800

    test RuntimeVisibleTypeAnnotationsAttribute

--- src/test/java/com/github/zxh/classpy/TypeAnnotatedClass.java
+++ src/test/java/com/github/zxh/classpy/TypeAnnotatedClass.java
@@ -3,10 +3,17 @@ package com.github.zxh.classpy;
 import java.util.ArrayList;
 
 @SuppressWarnings("serial")
-public class TypeAnnotatedClass<@MyTypeAnnotation/*targetType0x00*/ T> extends 
-        /*@MyTypeAnnotation*/ ArrayList</*@MyTypeAnnotation*/ T>  {
+public class TypeAnnotatedClass<@MyTypeAnnotation(strValue = "targetType0x00") T extends @MyTypeAnnotation(strValue = "targetType0x11") Object> extends 
+        @MyTypeAnnotation(strValue = "targetType0x10") ArrayList<T>  {
     
-    public <@MyTypeAnnotation T> void targetType0x01() {
+    @MyTypeAnnotation(strValue = "targetType0x13")
+    public int targetType0x13;
+    
+    public <@MyTypeAnnotation(strValue = "targetType0x01") T> void targetType0x01() {
+        
+    }
+    
+    public <T extends@MyTypeAnnotation(strValue = "targetType0x12") Object> void targetType0x12() {
         
     }
     
