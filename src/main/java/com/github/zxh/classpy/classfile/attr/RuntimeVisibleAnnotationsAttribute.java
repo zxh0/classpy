59d89e1d439d101977bcdd0682942c6750a6c3d1
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-15 13:13:57 +0800

    adjust comments

--- src/main/java/com/github/zxh/classpy/classfile/attr/RuntimeVisibleAnnotationsAttribute.java
+++ src/main/java/com/github/zxh/classpy/classfile/attr/RuntimeVisibleAnnotationsAttribute.java
@@ -5,6 +5,7 @@ import com.github.zxh.classpy.classfile.ClassReader;
 import com.github.zxh.classpy.classfile.Table;
 import com.github.zxh.classpy.classfile.U1;
 import com.github.zxh.classpy.classfile.U2;
+import com.github.zxh.classpy.classfile.U2CpIndex;
 import java.util.Arrays;
 import java.util.List;
 
@@ -14,31 +15,6 @@ RuntimeVisibleAnnotations_attribute {
     u4         attribute_length;
     u2         num_annotations;
     annotation annotations[num_annotations];
-}
-annotation {
-    u2 type_index;
-    u2 num_element_value_pairs;
-    {   u2            element_name_index;
-        element_value value;
-    } element_value_pairs[num_element_value_pairs];
-}
-element_value {
-    u1 tag;
-    union {
-        u2 const_value_index;
-
-        {   u2 type_name_index;
-            u2 const_name_index;
-        } enum_const_value;
-
-        u2 class_info_index;
-
-        annotation annotation_value;
-
-        {   u2            num_values;
-            element_value values[num_values];
-        } array_value;
-    } value;
 }
  */
 public class RuntimeVisibleAnnotationsAttribute extends AttributeInfo {
@@ -58,16 +34,24 @@ public class RuntimeVisibleAnnotationsAttribute extends AttributeInfo {
                 numAnnotations, annotations);
     }
     
-    
+    /*
+    annotation {
+        u2 type_index;
+        u2 num_element_value_pairs;
+        {   u2            element_name_index;
+            element_value value;
+        } element_value_pairs[num_element_value_pairs];
+    }
+    */
     public static class AnnotationInfo extends ClassComponent {
     
-        private U2 typeIndex;
+        private U2CpIndex typeIndex;
         private U2 numElementValuePairs;
         private Table<ElementValuePair> elementValuePairs;
     
         @Override
         protected void readContent(ClassReader reader) {
-            typeIndex = reader.readU2();
+            typeIndex = reader.readU2CpIndex();
             numElementValuePairs = reader.readU2();
             elementValuePairs = reader.readTable(ElementValuePair.class,
                     numElementValuePairs);
@@ -95,6 +79,26 @@ public class RuntimeVisibleAnnotationsAttribute extends AttributeInfo {
         
     }
     
+    /*
+    element_value {
+        u1 tag;
+        union {
+            u2 const_value_index;
+
+            {   u2 type_name_index;
+                u2 const_name_index;
+            } enum_const_value;
+
+            u2 class_info_index;
+
+            annotation annotation_value;
+
+            {   u2            num_values;
+                element_value values[num_values];
+            } array_value;
+        } value;
+    }
+    */
     public static class ElementValue extends ClassComponent {
 
         private U1 tag;
