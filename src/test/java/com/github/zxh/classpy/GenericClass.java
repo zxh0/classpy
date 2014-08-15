9400a11497c77dcf04b653f0bb1ff5fd8f31e378
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-15 12:53:16 +0800

    LocalVariableTypeTableAttribute

--- src/test/java/com/github/zxh/classpy/GenericClass.java
+++ src/test/java/com/github/zxh/classpy/GenericClass.java
@@ -1,6 +1,7 @@
 package com.github.zxh.classpy;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class GenericClass<T> {
@@ -9,6 +10,14 @@ public class GenericClass<T> {
     
     public static <T extends Comparable<T>> void m1(List<T> list) {
         //list.sort((a, b) -> 1);
+    }
+    
+    public void testLocalVariableTypeTableAttribute() {
+        List<String> ss = new ArrayList<>();
+        Comparator<Long> c = null;
+        int a = 1;
+        int b = 1;
+        int d = a + b;
     }
     
 }
