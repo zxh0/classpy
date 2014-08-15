3f67fcea8a63345e15215af404da517a31a2c4af
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-15 10:44:31 +0800

    refactor UnitTest

--- src/test/java/com/github/zxh/classpy/classfile/ClassFileTest.java
+++ src/test/java/com/github/zxh/classpy/classfile/ClassFileTest.java
@@ -10,11 +10,8 @@ import static org.junit.Assert.*;
 public class ClassFileTest {
     
     @Test
-    public void read() throws Exception {
-        String classFileName = TestClass.class.getName().replace('.', '/') + ".class";
-        Path classFilePath = Paths.get(TestClass.class.getClassLoader().getResource(classFileName).toURI());
-        byte[] classBytes = Files.readAllBytes(classFilePath);
-        
+    public void classFile() throws Exception {
+        byte[] classBytes = loadClass(TestClass.class);
         ClassFile cf = ClassParser.parse(classBytes);
         assertEquals(0, cf.getMinorVersion().getValue());
         assertEquals(52, cf.getMajorVersion().getValue());
@@ -28,10 +25,25 @@ public class ClassFileTest {
     @Test
     public void enclosingMethodAttribute() throws Exception {
         String classFileName = TestClass.class.getName().replace('.', '/') + "$1.class";
-        Path classFilePath = Paths.get(TestClass.class.getClassLoader().getResource(classFileName).toURI());
-        byte[] classBytes = Files.readAllBytes(classFilePath);
-        
+        byte[] classBytes = loadClass(classFileName);
         ClassParser.parse(classBytes);
+    }
+    
+    @Test
+    public void signatures() {
+        // todo
+    }
+    
+    private static byte[] loadClass(Class<?> cls) throws Exception {
+        String classFileName = cls.getName().replace('.', '/') + ".class";
+        return loadClass(classFileName);
+    }
+    
+    private static byte[] loadClass(String classFileName) throws Exception {
+        ClassLoader cl =TestClass.class.getClassLoader();
+        Path classFilePath = Paths.get(cl.getResource(classFileName).toURI());
+        byte[] classBytes = Files.readAllBytes(classFilePath);
+        return classBytes;
     }
     
 }
