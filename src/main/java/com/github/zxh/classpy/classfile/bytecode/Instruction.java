fb89d960c31531a82439d7ec965e5e5b375eba00
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-18 09:27:47 +0800

    add Instruction

---
+++ src/main/java/com/github/zxh/classpy/classfile/bytecode/Instruction.java
@@ -0,0 +1,17 @@
+package com.github.zxh.classpy.classfile.bytecode;
+
+import com.github.zxh.classpy.classfile.ClassComponent;
+import com.github.zxh.classpy.classfile.ClassReader;
+
+/**
+ *
+ * @author zxh
+ */
+public class Instruction extends ClassComponent {
+
+    @Override
+    protected void readContent(ClassReader reader) {
+        // todo
+    }
+    
+}
