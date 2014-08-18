bd0e9f7698b63e28877fa4bde6760c75850d6df5
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-18 17:23:16 +0800

    test iconst_x

--- src/test/java/com/github/zxh/classpy/testclasses/ByteCode.java
+++ src/test/java/com/github/zxh/classpy/testclasses/ByteCode.java
@@ -2,8 +2,19 @@ package com.github.zxh.classpy.testclasses;
 
 public class ByteCode {
     
-    public Object aconst_null() {
+    public Object aconst_null_areturn() {
         return null;
+    }
+    
+    public void iconst_x_return(int x) {
+        int a;
+        a = -1;
+        a = 0;
+        a = 1;
+        a = 2;
+        a = 3;
+        a = 4;
+        a = 5;
     }
     
 }
