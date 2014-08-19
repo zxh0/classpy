fddaea74d12132907505c4d3ce46c3df9c546968
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-19 20:34:32 +0800

    add hyperlink into about dialog

--- src/main/java/com/github/zxh/classpy/gui/AboutDialog.java
+++ src/main/java/com/github/zxh/classpy/gui/AboutDialog.java
@@ -1,6 +1,7 @@
 package com.github.zxh.classpy.gui;
 
 import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -12,15 +13,26 @@ import javafx.stage.Stage;
 public class AboutDialog {
     
     public static void showDialog() {
-        BorderPane pane = new BorderPane();
-        // todo
-        //return pane;
-        
         Stage dialogStage = new Stage();
         dialogStage.initModality(Modality.APPLICATION_MODAL);
         
-        dialogStage.setScene(new Scene(pane, 300, 400));
+        BorderPane aboutPane = createAboutPane(dialogStage);
+        Scene scene = new Scene(aboutPane, 300, 200);
+        
+        dialogStage.setScene(scene);
         dialogStage.show();
+    }
+    
+    private static BorderPane createAboutPane(Stage dialogStage) {
+        BorderPane pane = new BorderPane();
+        pane.setOnMouseClicked(e -> {
+            dialogStage.close();
+        });
+        
+        Hyperlink home = new Hyperlink("https://github.com/zxh0/classpy");
+        pane.setCenter(home);
+        
+        return pane;
     }
     
 }
