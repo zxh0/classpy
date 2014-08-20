52fbfaeaf11c2a5bbd27daa4e51ce759997db617
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-20 13:22:54 +0800

    add AccessFlags

---
+++ src/main/java/com/github/zxh/classpy/classfile/AccessFlags.java
@@ -0,0 +1,46 @@
+package com.github.zxh.classpy.classfile;
+
+/**
+ *
+ * @author zxh
+ */
+public class AccessFlags {
+    
+    private static final int TYPE_CLASS        = 0b0001;
+    private static final int TYPE_FIELD        = 0b0010;
+    private static final int TYPE_METHOD       = 0b0100;
+    private static final int TYPE_NESTED_CLASS = 0b1000;
+    private static final int TYPE_ALL          = 0b1111;
+    
+    private static enum Flags {
+        
+        ACC_PUBLIC      (0x0001, TYPE_ALL),
+        ACC_PRIVATE     (0x0002, TYPE_FIELD | TYPE_METHOD | TYPE_NESTED_CLASS),
+        ACC_PROTECTED   (0x0004, TYPE_FIELD | TYPE_METHOD | TYPE_NESTED_CLASS),
+        ACC_STATIC      (0x0008, TYPE_FIELD | TYPE_METHOD | TYPE_NESTED_CLASS),
+        ACC_FINAL       (0x0010, TYPE_ALL),
+        ACC_SUPER       (0x0020, TYPE_CLASS),
+        ACC_SYNCHRONIZED(0x0020, TYPE_METHOD),
+        ACC_VOLATILE    (0x0040, TYPE_FIELD),
+        ACC_BRIDGE      (0x0040, TYPE_METHOD),
+        ACC_TRANSIENT   (0x0080, TYPE_FIELD),
+        ACC_VARARGS     (0x0080, TYPE_METHOD),
+        ACC_NATIVE      (0x0100, TYPE_METHOD),
+        ACC_INTERFACE   (0x0200, TYPE_CLASS | TYPE_NESTED_CLASS),
+        ACC_ABSTRACT    (0x0400, TYPE_CLASS | TYPE_METHOD | TYPE_NESTED_CLASS),
+        ACC_STRICT      (0x0800, TYPE_METHOD),
+        ACC_SYNTHETIC   (0x1000, TYPE_ALL),
+        ACC_ANNOTATION  (0x2000, TYPE_CLASS | TYPE_NESTED_CLASS),
+        ACC_ENUM        (0x4000, TYPE_CLASS | TYPE_FIELD | TYPE_NESTED_CLASS); 
+        
+        private final int flag;
+        private final int type;
+        
+        private Flags(int flag, int type) {
+            this.flag = flag;
+            this.type = type;
+        }
+        
+    }
+    
+}
