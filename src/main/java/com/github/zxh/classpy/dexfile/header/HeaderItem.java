9149773c4f9565cf7d48cdba16ecdbb931725fda
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-21 15:38:21 +0800

    finish reading header

--- src/main/java/com/github/zxh/classpy/dexfile/header/HeaderItem.java
+++ src/main/java/com/github/zxh/classpy/dexfile/header/HeaderItem.java
@@ -17,6 +17,23 @@ public class HeaderItem extends DexComponent {
     private UInt fileSize;
     private UInt headerSize;
     private Hex endianTag;
+    private UInt linkSize;
+    private UInt linkOff;
+    private UInt mapOff;
+    private UInt stringIdsSize;
+    private UInt stringIdsOff;
+    private UInt typeIdsSize;
+    private UInt typeIdsOff;
+    private UInt protoIdsSize;
+    private UInt protoIdsOff;
+    private UInt fieldIdsSize;
+    private UInt fieldIdsOff;
+    private UInt methodIdsSize;
+    private UInt methodIdsOff;
+    private UInt classDefsSize;
+    private UInt classDefsOff;
+    private UInt dataSize;
+    private UInt dataOff;
     
     @Override
     protected void readContent(DexReader reader) {
@@ -27,7 +44,23 @@ public class HeaderItem extends DexComponent {
         fileSize = reader.readUInt();
         headerSize = reader.readUInt();
         endianTag = reader.readHex(4);
-        // todo
+        linkSize = reader.readUInt();
+        linkOff = reader.readUInt();
+        mapOff = reader.readUInt();
+        stringIdsSize = reader.readUInt();
+        stringIdsOff = reader.readUInt();
+        typeIdsSize = reader.readUInt();
+        typeIdsOff = reader.readUInt();
+        protoIdsSize = reader.readUInt();
+        protoIdsOff = reader.readUInt();
+        fieldIdsSize = reader.readUInt();
+        fieldIdsOff = reader.readUInt();
+        methodIdsSize = reader.readUInt();
+        methodIdsOff = reader.readUInt();
+        classDefsSize = reader.readUInt();
+        classDefsOff = reader.readUInt();
+        dataSize = reader.readUInt();
+        dataOff = reader.readUInt();
     }
     
 }
