c6ec248b17e45bea1f18377e46263c792d6c9292
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-21 13:41:41 +0800

    add DexFile

---
+++ src/main/java/com/github/zxh/classpy/dex/DexFile.java
@@ -0,0 +1,18 @@
+package com.github.zxh.classpy.dex;
+
+import com.github.zxh.classpy.dex.header.HeaderItem;
+
+/**
+ *
+ * @author zxh
+ */
+public class DexFile extends DexComponent {
+    
+    private HeaderItem header;
+
+    @Override
+    protected void readContent(DexReader reader) {
+        // todo
+    }
+    
+}
