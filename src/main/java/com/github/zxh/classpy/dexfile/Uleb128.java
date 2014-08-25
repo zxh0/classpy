81c5ff982e346e087e0934ac0ef1574c1a2cb8fc
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-25 17:56:50 +0800

    rename ULEB128 to Uleb128

---
+++ src/main/java/com/github/zxh/classpy/dexfile/Uleb128.java
@@ -0,0 +1,29 @@
+package com.github.zxh.classpy.dexfile;
+
+/**
+ * Unsigned Little-Endian Base 128.
+ *
+ * @author zxh
+ */
+public class Uleb128 extends DexComponent {
+
+    private int value;
+    
+    public int getValue() {
+        return value;
+    }
+    
+    @Override
+    protected void readContent(DexReader reader) {
+        for (int i = 0; i < 5; i++) {
+            final byte b = reader.readByte();
+            
+            value |= ((b & 0b0111_1111) << (i * 7));
+            
+            if ((b >> 7) == 0) {
+                break;
+            }
+        }
+    }
+    
+}
