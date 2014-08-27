b721163447e9e429eadf6453c18e9a3b6083d5a8
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-27 17:37:35 +0800

    display TypeItem

--- src/main/java/com/github/zxh/classpy/dexfile/data/TypeItem.java
+++ src/main/java/com/github/zxh/classpy/dexfile/data/TypeItem.java
@@ -2,7 +2,7 @@ package com.github.zxh.classpy.dexfile.data;
 
 import com.github.zxh.classpy.dexfile.DexComponent;
 import com.github.zxh.classpy.dexfile.DexReader;
-import com.github.zxh.classpy.dexfile.UShort;
+import com.github.zxh.classpy.dexfile.index.UShortTypeIdIndex;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,11 +12,11 @@ import java.util.List;
  */
 public class TypeItem extends DexComponent {
 
-    private UShort typeIdx;
+    private UShortTypeIdIndex typeIdx;
     
     @Override
     protected void readContent(DexReader reader) {
-        typeIdx = reader.readUShort();
+        typeIdx = reader.readUShortTypeIdIndex();
     }
 
     @Override
