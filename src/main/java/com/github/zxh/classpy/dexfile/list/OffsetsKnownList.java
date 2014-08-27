b4bee5395bae63806cbd1dfa301fe8d39ee3b6f0
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-27 11:40:56 +0800

    OffsetsKnownList extends DexList

--- src/main/java/com/github/zxh/classpy/dexfile/list/OffsetsKnownList.java
+++ src/main/java/com/github/zxh/classpy/dexfile/list/OffsetsKnownList.java
@@ -1,11 +1,8 @@
 package com.github.zxh.classpy.dexfile.list;
 
-import com.github.zxh.classpy.common.Util;
 import com.github.zxh.classpy.dexfile.DexComponent;
 import com.github.zxh.classpy.dexfile.DexReader;
 import com.github.zxh.classpy.dexfile.UInt;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -15,11 +12,10 @@ import java.util.stream.Stream;
  * 
  * @author zxh
  */
-public class OffsetsKnownList<E extends DexComponent> extends DexComponent {
+public class OffsetsKnownList<E extends DexComponent> extends DexList<E> {
 
     private final Stream<UInt> offStream;
     private final Supplier<E> factory;
-    private final List<E> list = new ArrayList<>();
 
     public OffsetsKnownList(Stream<UInt> offStream, Supplier<E> factory) {
         this.offStream = offStream;
@@ -27,38 +23,13 @@ public class OffsetsKnownList<E extends DexComponent> extends DexComponent {
     }
     
     @Override
-    protected void readContent(DexReader reader) {
-        readList(reader);
-        setElementName();
-    }
-    
-    private void readList(DexReader reader) {
+    protected void readList(DexReader reader) {
         offStream.forEach(offset -> {
             reader.setPosition(offset);
             E e = factory.get();
             e.read(reader);
             list.add(e);
         });
-    }
-    
-    // todo
-    private void setElementName() {
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            E element = list.get(i);
-            
-            String newName = Util.formatIndex(size, i);
-            String oldName = element.getName();
-            if (oldName != null) {
-                newName += " (" + oldName + ")";
-            }
-            element.setName(newName);
-        }
-    }
-    
-    @Override
-    public List<E> getSubComponents() {
-        return list;
     }
     
 }
