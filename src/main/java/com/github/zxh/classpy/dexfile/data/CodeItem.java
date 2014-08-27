1bde66c91d509bb780c765f4c62ce928c1d83032
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-27 18:16:24 +0800

    add CodeItem

--- src/main/java/com/github/zxh/classpy/dexfile/data/CodeItem.java
+++ src/main/java/com/github/zxh/classpy/dexfile/data/CodeItem.java
@@ -2,6 +2,10 @@ package com.github.zxh.classpy.dexfile.data;
 
 import com.github.zxh.classpy.dexfile.DexComponent;
 import com.github.zxh.classpy.dexfile.DexReader;
+import com.github.zxh.classpy.dexfile.UInt;
+import com.github.zxh.classpy.dexfile.UShort;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  *
@@ -9,9 +13,27 @@ import com.github.zxh.classpy.dexfile.DexReader;
  */
 public class CodeItem extends DexComponent {
 
+    private UShort registersSize;
+    private UShort insSize;
+    private UShort outsSize;
+    private UShort triesSize;
+    private UInt debugInfoOff;
+    private UInt insnsSize;
+    
     @Override
     protected void readContent(DexReader reader) {
-        // todo
+        registersSize = reader.readUShort();
+        insSize = reader.readUShort();
+        outsSize = reader.readUShort();
+        triesSize = reader.readUShort();
+        debugInfoOff = reader.readUInt();
+        insnsSize = reader.readUInt();
+    }
+
+    @Override
+    public List<? extends DexComponent> getSubComponents() {
+        return Arrays.asList(registersSize, insSize, outsSize, triesSize,
+                debugInfoOff, insnsSize);
     }
     
 }
