15fc62c3fef23f733c2a506ed7c1c7a0b02cff38
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-28 12:40:18 +0800

    use UIntHex

--- src/main/java/com/github/zxh/classpy/dexfile/data/MapItem.java
+++ src/main/java/com/github/zxh/classpy/dexfile/data/MapItem.java
@@ -4,6 +4,7 @@ import com.github.zxh.classpy.common.FileParseException;
 import com.github.zxh.classpy.dexfile.DexComponent;
 import com.github.zxh.classpy.dexfile.DexReader;
 import com.github.zxh.classpy.dexfile.datatype.UInt;
+import com.github.zxh.classpy.dexfile.datatype.UIntHex;
 import com.github.zxh.classpy.dexfile.datatype.UShort;
 import java.util.Arrays;
 import java.util.List;
@@ -17,14 +18,14 @@ public class MapItem extends DexComponent {
     private UShort type;
     private UShort unused;
     private UInt size;
-    private UInt offset;
+    private UIntHex offset;
     
     @Override
     protected void readContent(DexReader reader) {
         type = reader.readUShort();
         unused = reader.readUShort();
         size = reader.readUInt();
-        offset = reader.readUInt();
+        offset = reader.readUIntHex();
         setDesc(getTypeCode(type.getValue()));
     }
 
