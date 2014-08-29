38fb1370a169b95de9b001836c521d006d7dd837
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-29 17:45:56 +0800

    unit test

--- src/test/java/com/github/zxh/classpy/common/BytesReaderTest.java
+++ src/test/java/com/github/zxh/classpy/common/BytesReaderTest.java
@@ -1,9 +1,25 @@
 package com.github.zxh.classpy.common;
 
+import java.nio.ByteOrder;
+import java.util.stream.Stream;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
  *
  * @author zxh
  */
 public class BytesReaderTest {
+    
+    @Test
+    public void order() {
+        Stream.of(true, false).forEach(readOnly -> {
+            BytesReader be = new BytesReader(new byte[] {0x12, 0x34}, ByteOrder.BIG_ENDIAN, readOnly);
+            assertEquals(0x1234, be.getByteBuffer().getShort());
+
+            BytesReader le = new BytesReader(new byte[] {0x12, 0x34}, ByteOrder.LITTLE_ENDIAN, readOnly);
+            assertEquals(0x3412, le.getByteBuffer().getShort());
+        });
+    }
     
 }
