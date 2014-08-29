f45c9eeaeeda1dd93737fb1daebe30126db4ccef
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-29 09:36:44 +0800

    U4 implements IntValue

--- src/main/java/com/github/zxh/classpy/classfile/datatype/U4.java
+++ src/main/java/com/github/zxh/classpy/classfile/datatype/U4.java
@@ -2,16 +2,18 @@ package com.github.zxh.classpy.classfile.datatype;
 
 import com.github.zxh.classpy.classfile.ClassComponent;
 import com.github.zxh.classpy.classfile.ClassReader;
+import com.github.zxh.classpy.common.IntValue;
 
 /**
  * Unsigned four-byte quantity.
  * 
  * @author zxh
  */
-public class U4 extends ClassComponent {
+public class U4 extends ClassComponent implements IntValue {
 
     private int value;
 
+    @Override
     public int getValue() {
         return value;
     }
