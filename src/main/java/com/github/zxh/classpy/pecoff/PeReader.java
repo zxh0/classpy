6cd3fd96784df3b015dad49dc3c07d23b7787716
zxh0 <zhangxiuhong2013@gmail.com>
2014-08-29 21:31:52 +0800

    add UInt32

--- src/main/java/com/github/zxh/classpy/pecoff/PeReader.java
+++ src/main/java/com/github/zxh/classpy/pecoff/PeReader.java
@@ -3,6 +3,7 @@ package com.github.zxh.classpy.pecoff;
 import com.github.zxh.classpy.common.BytesReader;
 import com.github.zxh.classpy.pecoff.datatype.UInt16;
 import com.github.zxh.classpy.pecoff.datatype.UInt16Hex;
+import com.github.zxh.classpy.pecoff.datatype.UInt32;
 import java.nio.ByteOrder;
 
 /**
@@ -25,6 +26,13 @@ public class PeReader extends BytesReader {
     // todo
     public UInt16Hex readUInt16Hex() {
         UInt16Hex uint = new UInt16Hex();
+        uint.read(this);
+        return uint;
+    }
+    
+    // todo
+    public UInt32 readUInt32() {
+        UInt32 uint = new UInt32();
         uint.read(this);
         return uint;
     }
