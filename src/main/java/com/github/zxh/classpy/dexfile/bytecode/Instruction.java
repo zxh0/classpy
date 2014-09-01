ecfd1b9aed7193b93c74f6bfd1c619772faefd34
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-01 14:43:31 +0800

    add Instruction.java

---
+++ src/main/java/com/github/zxh/classpy/dexfile/bytecode/Instruction.java
@@ -0,0 +1,17 @@
+package com.github.zxh.classpy.dexfile.bytecode;
+
+import com.github.zxh.classpy.dexfile.DexComponent;
+import com.github.zxh.classpy.dexfile.DexReader;
+
+/**
+ *
+ * @author zxh
+ */
+public class Instruction extends DexComponent {
+
+    @Override
+    protected void readContent(DexReader reader) {
+        // todo
+    }
+    
+}
