e236b4081b9513257f56fdd23aad8ee988aed71e
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-07 18:30:39 +0800

    reading OptionalHeader...

--- src/main/java/com/github/zxh/classpy/pecoff/header/OptionalHeader.java
+++ src/main/java/com/github/zxh/classpy/pecoff/header/OptionalHeader.java
@@ -86,6 +86,8 @@ public class OptionalHeader extends PeComponent {
         private UInt32 sizeOfImage;
         private UInt32 sizeOfHeaders;
         private UInt32 checkSum;
+        private UInt16 subsystem;
+        private UInt16Hex dllCharacteristics;
         // todo
         
         public WindowsSpecificFields(boolean isPE32Plus) {
@@ -114,11 +116,31 @@ public class OptionalHeader extends PeComponent {
             sizeOfImage = reader.readUInt32();
             sizeOfHeaders = reader.readUInt32();
             checkSum = reader.readUInt32();
+            subsystem = reader.readUInt16();
+            subsystem.setDesc(subsystem.getDesc() + "(" + getSubsystem(subsystem.getValue()) + ")");
+            dllCharacteristics = reader.readUInt16Hex();
         }
         
         private void describeAlignment(UInt32 alignment) {
             if (alignment.getValue() > 1024) {
                 alignment.setDesc((alignment.getValue() / 1024) + "K");
+            }
+        }
+        
+        private static String getSubsystem(int value) {
+            switch (value) {
+                case 0: return "IMAGE_SUBSYSTEM_UNKNOWN"; // An unknown subsystem
+                case 1: return "IMAGE_SUBSYSTEM_NATIVE"; // Device drivers and native Windows processes
+                case 2: return "IMAGE_SUBSYSTEM_WINDOWS_GUI"; // The Windows graphical user interface (GUI) subsystem
+                case 3: return "IMAGE_SUBSYSTEM_WINDOWS_CUI"; // The Windows character subsystem
+                case 7: return "IMAGE_SUBSYSTEM_POSIX_CUI"; // The Posix character subsystem
+                case 9: return "IMAGE_SUBSYSTEM_WINDOWS_CE_GUI"; // Windows CE
+                case 10: return "IMAGE_SUBSYSTEM_EFI_APPLICATION"; // An Extensible Firmware Interface (EFI) application
+                case 11: return "IMAGE_SUBSYSTEM_EFI_BOOT_SERVICE_DRIVER"; // An EFI driver with boot services
+                case 12: return "IMAGE_SUBSYSTEM_EFI_RUNTIME_DRIVER"; // An EFI driver with run-time services
+                case 13: return "IMAGE_SUBSYSTEM_EFI_ROM"; // An EFI ROM image
+                case 14: return "IMAGE_SUBSYSTEM_XBOX"; // XBOX
+                default: throw new FileParseException("Invalid Subsystem: " + value);
             }
         }
         
