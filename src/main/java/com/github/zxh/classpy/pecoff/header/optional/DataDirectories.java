3de3a903b5519e722340f6fda31cd5f120f0f7bd
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-07 21:51:56 +0800

    add class: DataDirectory

--- src/main/java/com/github/zxh/classpy/pecoff/header/optional/DataDirectories.java
+++ src/main/java/com/github/zxh/classpy/pecoff/header/optional/DataDirectories.java
@@ -2,6 +2,7 @@ package com.github.zxh.classpy.pecoff.header.optional;
 
 import com.github.zxh.classpy.pecoff.PeComponent;
 import com.github.zxh.classpy.pecoff.PeReader;
+import com.github.zxh.classpy.pecoff.datatype.UInt32;
 
 /**
  *
@@ -14,4 +15,18 @@ public class DataDirectories extends PeComponent {
         // todo
     }
 
+    
+    public static class DataDirectory extends PeComponent {
+
+        private UInt32 virtualAddress;
+        private UInt32 size;
+        
+        @Override
+        protected void readContent(PeReader reader) {
+            virtualAddress = reader.readUInt32();
+            size = reader.readUInt32();
+        }
+        
+    }
+    
 }
