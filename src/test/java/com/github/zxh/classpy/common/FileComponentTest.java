f2bdd414ac533d8a257733660c9d867ba4f631ba
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-07 18:32:50 +0800

    add FileComponentTest

---
+++ src/test/java/com/github/zxh/classpy/common/FileComponentTest.java
@@ -0,0 +1,16 @@
+package com.github.zxh.classpy.common;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author zxh
+ */
+public class FileComponentTest {
+    
+    @Test
+    public void _toString() {
+        // todo
+    }
+    
+}
