0376f31998e544ee203a0b0b6f4b1bc990f8c604
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-08 19:43:19 +0800

    add SectionName

---
+++ src/main/java/com/github/zxh/classpy/pecoff/header/section/SectionName.java
@@ -0,0 +1,17 @@
+package com.github.zxh.classpy.pecoff.header.section;
+
+import com.github.zxh.classpy.pecoff.PeComponent;
+import com.github.zxh.classpy.pecoff.PeReader;
+
+/**
+ *
+ * @author zxh
+ */
+public class SectionName extends PeComponent {
+
+    @Override
+    protected void readContent(PeReader reader) {
+        // todo
+    }
+    
+}
