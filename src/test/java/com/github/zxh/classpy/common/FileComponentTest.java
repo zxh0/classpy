81a57cbafe44a0ecb7a5ba9116764baa4d809da9
zxh0 <zhangxiuhong2013@gmail.com>
2014-09-09 14:10:55 +0800

    test FileComponent.toString()

--- src/test/java/com/github/zxh/classpy/common/FileComponentTest.java
+++ src/test/java/com/github/zxh/classpy/common/FileComponentTest.java
@@ -1,6 +1,7 @@
 package com.github.zxh.classpy.common;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  *
@@ -10,7 +11,22 @@ public class FileComponentTest {
     
     @Test
     public void _toString() {
-        // todo
+        // name == null && desc == null
+        FileComponent fc = new FileComponent();
+        assertEquals(FileComponent.class.getSimpleName(), fc.toString());
+        
+        // desc == null
+        fc.setName("nama");
+        assertEquals("nama", fc.toString());
+        
+        // name == null
+        fc.setName(null);
+        fc.setDesc("dasc");
+        assertEquals("dasc", fc.toString());
+        
+        fc.setName("nama");
+        fc.setDesc("dasc");
+        assertEquals("nama: dasc", fc.toString());
     }
     
 }
