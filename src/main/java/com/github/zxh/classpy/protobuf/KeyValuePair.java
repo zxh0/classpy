b4cfc802c1467fe8a10aaaa2188da35d8430a574
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-07 15:32:14 +0800

    read variant

--- src/main/java/com/github/zxh/classpy/protobuf/KeyValuePair.java
+++ src/main/java/com/github/zxh/classpy/protobuf/KeyValuePair.java
@@ -6,10 +6,28 @@ package com.github.zxh.classpy.protobuf;
  */
 public class KeyValuePair extends PbComponent {
 
+    private Key key;
+    
     @Override
     protected void readContent(PbReader reader) {
         // todo
-        reader.readByte();
+        key = new Key();
+        key.read(reader);
+    }
+    
+    public static class Key extends PbComponent {
+
+        private int wireType;
+        private int tag;
+        
+        @Override
+        protected void readContent(PbReader reader) {
+            int varint = reader.readVarint();
+            wireType = varint & 0b111;
+            tag = varint >>> 3;
+            //setDesc(wireType+":"+tag);
+        }
+        
     }
     
 }
--- src/main/java/com/github/zxh/classpy/protobuf/PbReader.java
+++ src/main/java/com/github/zxh/classpy/protobuf/PbReader.java
@@ -13,4 +13,22 @@ public class PbReader extends BytesReader {
         super(bytes, ByteOrder.LITTLE_ENDIAN, true);
     }
     
+    // Base 128 Varints
+    public int readVarint() {
+        int value = 0;
+        
+        for (int i = 0; i < 5; i++) {
+            final byte b = readByte();
+            
+            value |= ((b & 0b0111_1111) << (i * 7));
+            
+            if (b >= 0) {
+                // most significant bit clear
+                break;
+            }
+        }
+        
+        return value;
+    }
+    
 }
