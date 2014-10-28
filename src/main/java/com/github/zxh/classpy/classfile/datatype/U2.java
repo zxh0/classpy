e4bd430c23ed9d05cb9fb948f4b5c6338fd77fd9
zxh0 <zhangxiuhong2013@gmail.com>
2014-10-28 10:23:37 +0800

    U2 extends UInt

--- src/main/java/com/github/zxh/classpy/classfile/datatype/U2.java
+++ src/main/java/com/github/zxh/classpy/classfile/datatype/U2.java
@@ -1,31 +1,17 @@
 package com.github.zxh.classpy.classfile.datatype;
 
-import com.github.zxh.classpy.classfile.ClassComponent;
 import com.github.zxh.classpy.classfile.ClassReader;
-import com.github.zxh.classpy.common.IntValue;
 
 /**
  * Unsigned two-byte quantity.
  *
  * @author zxh
  */
-public class U2 extends ClassComponent implements IntValue {
-
-    private int value;
+public class U2 extends UInt {
 
     @Override
-    public int getValue() {
-        return value;
-    }
-    
-    @Override
-    protected void readContent(ClassReader reader) {
-        value = reader.readUnsignedShort();
-        describe(value, reader);
-    }
-    
-    protected void describe(int value, ClassReader reader) {
-        setDesc(value);
+    protected int readUInt(ClassReader reader) {
+        return reader.readUnsignedShort();
     }
     
 }
