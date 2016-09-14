31afce50f4c892656b93e37ee813bc681599ee80
zxh0 <zhangxiuhong2013@gmail.com>
2016-09-14 15:08:13 +0800

    UINT

--- src/main/java/com/github/zxh/classpy/classfile/datatype/UInt.kt
+++ src/main/java/com/github/zxh/classpy/classfile/datatype/UInt.kt
@@ -2,25 +2,37 @@ package com.github.zxh.classpy.classfile.datatype;
 
 import com.github.zxh.classpy.classfile.ClassComponent;
 import com.github.zxh.classpy.classfile.constant.ConstantPool;
+import com.github.zxh.classpy.classfile.reader.ClassReader
+import com.github.zxh.classpy.helper.StringHelper
 
-/**
- *              UInt
- *            /  |   \
- *          U1   U2   U4
- *         /     |      \
- *  U1CpIndex U2CpIndex U4Hex
- */
-public abstract class UInt extends ClassComponent {
+open class UInt(
+        readInt: (reader: ClassReader) -> Int,
+        describeInt: (i: Int, cp: ConstantPool) -> String
+    ) : ClassComponent() {
 
-    protected int value;
-    
-    public final int getValue() {
-        return value;
+    private val _readInt = readInt
+    private val _describeInt = describeInt
+    var value: Int = 0
+
+    override  fun readContent(reader: ClassReader) {
+        value = _readInt(reader)
     }
 
-    @Override
-    protected void afterRead(ConstantPool cp) {
-        setDesc(Integer.toString(value));
+    override fun afterRead(cp: ConstantPool) {
+        desc = _describeInt(value, cp)
     }
     
 }
+
+fun int2String(i: Int, cp: ConstantPool): String = Integer.toString(i)
+fun int2Hex(i: Int, cp: ConstantPool): String = StringHelper.toHexString(i)
+fun int2CpIndex(i: Int, cp: ConstantPool): String {
+    return if (i > 0) "#$i->${cp.getConstantDesc(i)}" else "#$i"
+}
+
+class U1 :       UInt(ClassReader::readUnsignedByte,  ::int2String)
+class U1CpIndex: UInt(ClassReader::readUnsignedByte,  ::int2CpIndex)
+class U2 :       UInt(ClassReader::readUnsignedShort, ::int2String)
+class U2CpIndex: UInt(ClassReader::readUnsignedShort, ::int2CpIndex)
+class U4 :       UInt(ClassReader::readInt,           ::int2String) // TODO
+class U4Hex :    UInt(ClassReader::readInt,           ::int2Hex)
