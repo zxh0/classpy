42bc34d6c621fdd4e4ed9da018df05052e185153
zxh0 <zhangxiuhong2013@gmail.com>
2016-09-15 10:14:53 +0800

    InstructionU1

--- src/main/java/com/github/zxh/classpy/classfile/bytecode/InstructionU1.kt
+++ src/main/java/com/github/zxh/classpy/classfile/bytecode/InstructionU1.kt
@@ -3,13 +3,13 @@ package com.github.zxh.classpy.classfile.bytecode
 import com.github.zxh.classpy.classfile.constant.ConstantPool
 
 /**
- * The instruction whose operand is U1CpIndex.
+ * The instruction whose operand is U1.
  */
-class InstructionCp1(opcode: Opcode, pc: Int) : Instruction(opcode, pc) {
+class InstructionU1(opcode: Opcode, pc: Int) : Instruction(opcode, pc) {
 
     init {
-        u1  ("opcode")
-        u1cp("operand")
+        u1("opcode")
+        u1("operand")
     }
 
     override fun afterRead(cp: ConstantPool) {
