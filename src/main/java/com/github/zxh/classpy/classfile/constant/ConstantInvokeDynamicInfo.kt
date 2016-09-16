5454705845b0b8ac9a10a06e8aede2871d1583bc
zxh0 <zhangxiuhong2013@gmail.com>
2016-09-16 19:36:06 +0800

    ConstantInvokeDynamicInfo

--- src/main/java/com/github/zxh/classpy/classfile/constant/ConstantInvokeDynamicInfo.kt
+++ src/main/java/com/github/zxh/classpy/classfile/constant/ConstantInvokeDynamicInfo.kt
@@ -7,16 +7,15 @@ CONSTANT_InvokeDynamic_info {
     u2 name_and_type_index;
 }
 */
-public class ConstantInvokeDynamicInfo extends ConstantInfo {
+class ConstantInvokeDynamicInfo : ConstantInfo() {
 
-    {
+    init {
         u2("bootstrap_method_attr_index");
         u2("name_and_type_index");
     }
     
-    @Override
-    protected String loadDesc(ConstantPool cp) {
-        int nameAndTypeIndex = super.getInt("name_and_type_index");
+    override fun loadDesc(cp: ConstantPool): String {
+        val nameAndTypeIndex = super.getInt("name_and_type_index");
         return cp.getNameAndTypeInfo(nameAndTypeIndex).loadDesc(cp);
     }
     
