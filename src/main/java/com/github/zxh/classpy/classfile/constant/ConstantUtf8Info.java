bc5dd26bca763e140e79034ecb0f1caaf1cfb316
zxh0 <zhangxiuhong2013@gmail.com>
2016-09-18 18:18:48 +0800

    delete Mutf8.java

--- src/main/java/com/github/zxh/classpy/classfile/constant/ConstantUtf8Info.java
+++ src/main/java/com/github/zxh/classpy/classfile/constant/ConstantUtf8Info.java
@@ -1,7 +1,13 @@
 package com.github.zxh.classpy.classfile.constant;
 
+import com.github.zxh.classpy.classfile.ClassComponent;
+import com.github.zxh.classpy.classfile.ClassParseException;
 import com.github.zxh.classpy.classfile.datatype.U2;
+import com.github.zxh.classpy.classfile.reader.ClassReader;
+import com.github.zxh.classpy.helper.Mutf8Decoder;
 import com.github.zxh.classpy.helper.StringHelper;
+
+import java.io.IOException;
 
 /*
 CONSTANT_Utf8_info {
@@ -20,7 +26,7 @@ public class ConstantUtf8Info extends ConstantInfo {
     }
 
     public String getString() {
-        return ((Mutf8) super.get("bytes")).getValue();
+        return ((Mutf8) super.get("bytes")).str;
     }
 
     @Override
@@ -28,5 +34,30 @@ public class ConstantUtf8Info extends ConstantInfo {
         Mutf8 bytes = (Mutf8) super.get("bytes");
         return StringHelper.cutAndAppendEllipsis(bytes.getDesc(), 100);
     }
-    
+
+
+    // UTF8 String in constant pool.
+    private class Mutf8 extends ClassComponent {
+
+        private final U2 length;
+        private String str;
+
+        public Mutf8(U2 length) {
+            this.length = length;
+        }
+
+        @Override
+        protected void readContent(ClassReader reader) {
+            byte[] bytes = reader.readBytes(length.getValue());
+            try {
+                str = Mutf8Decoder.decodeMutf8(bytes);
+            } catch (IOException e) {
+                throw new ClassParseException(e);
+            }
+
+            setDesc(str);
+        }
+
+    }
+
 }
