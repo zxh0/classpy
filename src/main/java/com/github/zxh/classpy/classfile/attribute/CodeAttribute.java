834e05955bf43fd0e71ac802891376cbf16dfa61
zxh0 <zhangxiuhong2013@gmail.com>
2016-09-18 18:44:59 +0800

    delete Code.java

--- src/main/java/com/github/zxh/classpy/classfile/attribute/CodeAttribute.java
+++ src/main/java/com/github/zxh/classpy/classfile/attribute/CodeAttribute.java
@@ -1,7 +1,14 @@
 package com.github.zxh.classpy.classfile.attribute;
 
 import com.github.zxh.classpy.classfile.ClassComponent;
+import com.github.zxh.classpy.classfile.bytecode.Instruction;
+import com.github.zxh.classpy.classfile.bytecode.InstructionFactory;
+import com.github.zxh.classpy.classfile.bytecode.Opcode;
+import com.github.zxh.classpy.classfile.constant.ConstantPool;
 import com.github.zxh.classpy.classfile.datatype.U4;
+import com.github.zxh.classpy.classfile.reader.ClassReader;
+
+import java.util.List;
 
 /*
 Code_attribute {
@@ -47,5 +54,45 @@ public class CodeAttribute extends AttributeInfo {
         }
 
     }
-    
+
+
+    private class Code extends ClassComponent {
+
+        private final U4 codeLength;
+
+        public Code(U4 codeLength) {
+            this.codeLength = codeLength;
+        }
+
+        @Override
+        protected void readContent(ClassReader reader) {
+            final int startPosition = reader.getPosition();
+            final int endPosition = startPosition + codeLength.getValue();
+
+            int position;
+            while ((position = reader.getPosition()) < endPosition) {
+                int pc = position - startPosition;
+                byte b = reader.getByte(position);
+                Opcode opcode = Opcode.valueOf(Byte.toUnsignedInt(b));
+                Instruction instruction = InstructionFactory.create(opcode, pc);
+                instruction.read(reader);
+                add(instruction);
+            }
+        }
+
+        @Override
+        protected void afterRead(ConstantPool cp) {
+            List<ClassComponent> instructions = super.getComponents();
+
+            int maxPc = ((Instruction) instructions.get(instructions.size() - 1)).getPc();
+            int pcWidth = String.valueOf(maxPc).length();
+            String fmtStr = "%0" + pcWidth + "d";
+            for (ClassComponent c : instructions) {
+                Instruction instruction = (Instruction) c;
+                instruction.setName(String.format(fmtStr, instruction.getPc()));
+            }
+        }
+
+    }
+
 }
